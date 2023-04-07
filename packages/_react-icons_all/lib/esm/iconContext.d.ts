fe29a88ff69411055b54bcd1044d593a86685e63
cavinHuang <huangchunmao1993@gmail.com>
2023-04-07 17:10:36 +0800

    feat: 删除不必要的文件

--- packages/_react-icons_all/lib/esm/iconContext.d.ts
+++
@@ -1,9 +0,0 @@
-import { CSSProperties, SVGAttributes } from "vue";
-export interface IconContext {
-    color?: string;
-    size?: string;
-    className?: string;
-    style?: CSSProperties;
-    attr?: SVGAttributes;
-}
-export declare const DefaultContext: IconContext;
--- packages/_react-icons_all/lib/esm/iconContext.js
+++
@@ -1,8 +0,0 @@
-export var DefaultContext = {
-    color: undefined,
-    size: undefined,
-    className: undefined,
-    style: undefined,
-    attr: undefined,
-};
-//export const IconContext: React.Context<IconContext> = React.createContext && React.createContext(DefaultContext);
