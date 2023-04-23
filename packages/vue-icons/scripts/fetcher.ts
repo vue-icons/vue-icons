bc3a06c1d9b5b5d9c5e33407474ad90046844617
cavinHuang <huangchunmao1993@gmail.com>
2023-04-23 15:49:21 +0800

    222

--- packages/vue-icons/scripts/fetcher.ts
+++ packages/vue-icons/scripts/fetcher.ts
@@ -12,14 +12,8 @@ interface Context {
   iconDir(name: string): string;
 }
 
-const sucessRepo = path.resolve(__dirname, '../successRepo.txt')
-const failedRepo = path.resolve(__dirname, '../faildedRepo.txt')
-fs.appendFileSync(sucessRepo, '')
-fs.appendFileSync(failedRepo, '')
-
 async function main() {
   const distBaseDir = path.join(__dirname, "../icons");
-  console.log(distBaseDir)
   const ctx: Context = {
     distBaseDir,
     iconDir(name: string) {
@@ -28,36 +22,20 @@ async function main() {
   };
 
   // rm all icons and mkdir dist
-  /*await fs.promises.rm(distBaseDir, {
+  await fs.promises.rm(distBaseDir, {
     recursive: true,
     force: true,
   });
   await fs.promises.mkdir(distBaseDir, {
     recursive: true,
-  });*/
+  });
 
   const queue = new PQueue({ concurrency: 10 });
-  const sucessString = fs.readFileSync(sucessRepo).toString()
-  const sucesses = sucessString.split('\r\n')
-  const length = Object.keys(icons).length
-  let _index = 0
-  console.log(sucesses)
   for (const icon of icons) {
-    console.log(icon.name, icon.source?.localName)
-    if (sucesses.includes(icon.source?.localName || '')) {
-      console.log(`第${_index}个已经下载成功,跳过,总个数${length}`);
-      _index++
-      continue
-    }
     if (!icon.source) {
-      console.log(`第${_index}个没有信息,跳过,总个数${length}`);
-      _index
       continue;
     }
     const { source } = icon;
-    source.index = _index
-    source.totalRepo = length
-    _index++
     queue.add(() => gitCloneIcon(source, ctx));
   }
 
@@ -66,7 +44,7 @@ async function main() {
 
 async function gitCloneIcon(source: IconSetGitSource, ctx: Context) {
   console.log(
-    `start ${source.index} clone icon: ${source.url}/${source.remoteDir}@${source.branch}`
+    `start clone icon: ${source.url}/${source.remoteDir}@${source.branch}`
   );
   await execFile(
     "git",
@@ -74,11 +52,7 @@ async function gitCloneIcon(source: IconSetGitSource, ctx: Context) {
     {
       cwd: ctx.distBaseDir,
     }
-  ).catch((e) => {
-    console.log(`git clone faild ${source.index}个，${source.localName}\n\n${e}`)
-    console.error(e)
-    throw { source, ctx };
-  });
+  );
 
   await execFile(
     "git",
@@ -86,28 +60,14 @@ async function gitCloneIcon(source: IconSetGitSource, ctx: Context) {
     {
       cwd: ctx.iconDir(source.localName),
     }
-  ).catch((e) => {
-    console.log(`git checkout faild ${source.index}个，${source.localName}\n\n${e}`)
-    console.error(e)
-
-    throw { source, ctx };
-  });;
+  );
 
   await execFile("git", ["checkout", source.hash], {
     cwd: ctx.iconDir(source.localName),
-  }).catch((e) => {
-    console.log(`git checkout hash faild ${source.index}个，${source.localName}\n${source.hash}\n\n${e}`)
-    console.error(e)
-    throw { source, ctx };
   });
-  console.log(`第${source.index}个已经成功，总个数${source.totalRepo}`)
-  fs.appendFileSync(sucessRepo, `${source.localName}
-`)
 }
 
 main().catch((err) => {
   console.error(err);
-  fs.appendFileSync(failedRepo, `${err.source.localName}
-`)
-  // process.exit(1);
+  process.exit(1);
 });
