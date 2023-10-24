c276556b73023bc3665dc862eaf712f8ce510868
cavinHuang <huangchunmao1993@gmail.com>
2023-10-24 15:58:29 +0800

    chore(release): publish v0.1.1

--- packages/vue-icons/scripts/build.ts
+++ packages/vue-icons/scripts/build.ts
@@ -6,14 +6,14 @@ import * as taskAll from "./task_all";
 import * as taskFiles from "./task_files";
 
 // file path
-const _rootDir = path.resolve(__dirname, "../");
+const _rootDir = path.resolve(__dirname,"../");
 
-async function task(name, fn) {
+async function task(name,fn) {
   const start = performance.now();
   console.log(`================= ${name} =================`);
   await fn();
   const end = performance.now();
-  console.log(`${name}: `, Math.floor(end - start) / 1000, "sec\n\n");
+  console.log(`${name}: `,Math.floor(end - start) / 1000,"sec\n\n");
 }
 
 async function main() {
@@ -21,30 +21,38 @@ async function main() {
     // @vue-icons/all
     const allOpt = {
       rootDir: _rootDir,
-      DIST: path.resolve(_rootDir, "../_vue-icons_all"),
-      LIB: path.resolve(_rootDir, "../_vue-icons_all/lib"),
+      DIST: path.resolve(_rootDir,"../_vue-icons_all"),
+      LIB: path.resolve(_rootDir,"../_vue-icons_all/lib"),
     };
-    await task("@vue3-icons/all initialize", async () => {
+    await task("@vue3-icons/all initialize",async () => {
       await taskAll.dirInit(allOpt);
       await taskCommon.writeEntryPoints(allOpt);
       await taskCommon.writeIconsManifest(allOpt);
       await taskCommon.writeLicense(allOpt);
-      await taskCommon.writePackageJson({ name: "vue3-icons" }, allOpt);
+      await taskCommon.writePackageJson({
+        name: "vue3-icons",homePage: "https://vue-icons.github.io/vue-icons/","bugs": {
+          "url": "https://github.com/vue-icons/vue-icons/issues"
+        },
+        "repository": {
+          "type": "git",
+          "url": "git+https://github.com/vue-icons/vue-icons.git"
+        },
+      },allOpt);
       await taskCommon.copyReadme(allOpt);
     });
-    await task("@vue3-icons/all write icons", async () => {
+    await task("@vue3-icons/all write icons",async () => {
       for (const icon of icons) {
-        await taskAll.writeIconModule(icon, allOpt);
+        await taskAll.writeIconModule(icon,allOpt);
       }
     });
 
     // @vue3-icons/all-files
     const filesOpt = {
       rootDir: _rootDir,
-      DIST: path.resolve(_rootDir, "../_vue-icons_all-files"),
-      LIB: path.resolve(_rootDir, "../_vue-icons_all-files/lib"),
+      DIST: path.resolve(_rootDir,"../_vue-icons_all-files"),
+      LIB: path.resolve(_rootDir,"../_vue-icons_all-files/lib"),
     };
-    await task("@vue3-icons/all-files initialize", async () => {
+    await task("@vue3-icons/all-files initialize",async () => {
       await taskFiles.dirInit(filesOpt);
       await taskCommon.writeEntryPoints(filesOpt);
       await taskCommon.writeIconsManifest(filesOpt);
@@ -55,19 +63,19 @@ async function main() {
       );
       await taskCommon.copyReadme(filesOpt);
     });
-    await task("@vue3-icons/all-files write icons", async () => {
+    await task("@vue3-icons/all-files write icons",async () => {
       for (const icon of icons) {
-        await taskFiles.writeIconModuleFiles(icon, filesOpt);
+        await taskFiles.writeIconModuleFiles(icon,filesOpt);
       }
     });
 
     // write to VERSIONS file
-    await task("vue-icons_builders write icon versions", async () => {
+    await task("vue-icons_builders write icon versions",async () => {
       await taskCommon.writeIconVersions(filesOpt);
     });
 
     // write to d.ts files
-    await task("vue-icons_builders build common library", async () => {
+    await task("vue-icons_builders build common library",async () => {
       await taskCommon.buildLib(filesOpt);
       await taskCommon.copyLib(allOpt);
       await taskCommon.copyLib(filesOpt);
@@ -81,4 +89,4 @@ async function main() {
 }
 main();
 
-export {};
+export { };
