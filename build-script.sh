130e1412b54d710bc01b6a19e4cae0c2a36848ac
cavinHuang <huangchunmao1993@gmail.com>
2023-10-25 15:11:06 +0800

    chore(release): publish v0.1.9

--- build-script.sh
+++ build-script.sh
@@ -3,7 +3,7 @@
 time (cd packages/vue-icons/ && pnpm run fetch)
 time (cd packages/vue-icons/ && pnpm run build)
 echo VERSIONS; cat packages/vue-icons/VERSIONS
-(cd packages/_vue-icons_all/ && npm pack |& tail)
+(cd packages/_vue-icons_all/ && pnpm pack |& tail)
 (cd packages/_vue-icons_all-files/ && pnpm pack |& tail)
 
 (cd packages/vue3-demo/ && pnpm run build)
